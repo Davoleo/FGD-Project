4141ee19d8e328e49cfe6c91e34c5d6098f75e19
davoleo <dav@davoleo.net>
2026-05-05 01:07:41 +0200

    Sorta fix doors

--- Assets/Scripts/Triggers/DoorTriggerHandler.cs
+++ Assets/Scripts/Triggers/DoorTriggerHandler.cs
@@ -7,27 +7,48 @@ namespace Triggers
     public class DoorTriggerHandler : MonoBehaviour
     {
         private CollectiblesManager manager;
-
         private PlayerInputHandler inputHandler = null;
+
+        private bool isClose;
 
         private void Start()
         {
             manager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<CollectiblesManager>();
         }
 
-        private void OnTriggerStay(Collider other)
+        private void Update()
+        {
+            if (isClose)
+            {
+                Debug.Log(inputHandler.interactAction.action.triggered);
+                if (inputHandler.interactAction.action.triggered && manager.UseKey())
+                {
+                    gameObject.SetActive(false);
+                }
+            }
+        }
+
+        private void OnTriggerExit(Collider other)
         {
             if (other.gameObject.CompareTag("Player"))
             {
                 if (inputHandler == null)
                     inputHandler = other.gameObject.GetComponent<PlayerInputHandler>();
 
+                Debug.Log("Exit");
+                isClose = false;
+            }
+        }
 
-                Debug.Log(inputHandler.interactAction.action.triggered + " " + manager.keys);
-                if (inputHandler.interactAction.action.triggered && manager.UseKey())
-                {
-                    gameObject.SetActive(false);
-                }
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.CompareTag("Player"))
+            {
+                if (inputHandler == null)
+                    inputHandler = other.gameObject.GetComponent<PlayerInputHandler>();
+
+                Debug.Log("Enter");
+                isClose = true;
             }
         }
     }
